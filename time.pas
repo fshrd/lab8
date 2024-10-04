560e6f81b8cd357ea5396a4bb8ef342ee328274a
local user <user@mil8a-102a-08.mmcs.sfedu.ru>
2024-10-04 13:25:42 +0300

    Initial commit

---
+++ time.pas
@@ -0,0 +1,12 @@
+﻿begin
+  var year:= readinteger;
+  var vis:= True;
+  
+  if year mod 4 = 0 then begin
+    if (year mod 100 = 0) and (year mod 400 <> 0) then
+      vis := False;
+    end
+  else
+    vis := False;
+  print(vis)
+end.
