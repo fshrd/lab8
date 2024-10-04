2eaac27789ea987065574b078c5bd528480de5da
local user <user@mil8a-102a-08.mmcs.sfedu.ru>
2024-10-04 13:17:57 +0300

    23

--- hello.pas
+++ hello.pas
@@ -1,4 +1,10 @@
 ﻿begin
-  var s:= 'Привет, ыыыаыыыааыыаа!';
-  print(s)
+  var s:= readinteger;
+  Assert(s in 0..24);
+  case s of
+    4..10: println('Доброе утро, мир!');
+    11..16: println('Добрый день, мир!');
+    17..22: println('Доообрейший вечерочек, мир!');
+    else println('Доброй ночи, мир!');
+  end;
 end.
